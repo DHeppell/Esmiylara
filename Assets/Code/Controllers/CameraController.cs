5afd56dbd069b5c65d279ec8a211da977e4660ef
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2018-04-03 11:02:46 -0400

    Camera now follows the player.

--- Assets/Code/Controllers/CameraController.cs
+++ Assets/Code/Controllers/CameraController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    public GameObject Target;
+
 	// Use this for initialization
 	void Start () {
-		
+        Target = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Camera.main.orthographicSize = Screen.height / 2 / 32;
+
+        if(Target != null)
+        {
+            transform.position = new Vector3(
+                Target.transform.position.x, Target.transform.position.y, -10);
+        }
+        else
+        {
+            transform.position = new Vector3(0, 0, -10);
+        }
 	}
 }
