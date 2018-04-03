4113caa57ee30c2883a2fa1110374871f4292a9c
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2018-04-03 11:13:05 -0400

    PixelsPerUnit constant added.

---
+++ Assets/Code/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace Esmiylara
+{
+    /// <summary>
+    /// Defines game constants.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Defines graphics constants.
+        /// </summary>
+        public static class Graphics
+        {
+            /// <summary>
+            /// Defines the number of pixels per game unit, your sprites ppu should match this.
+            /// </summary>
+            public const int PixelsPerUnit = 32;
+        }
+    }
+}
--- Assets/Code/Controllers/CameraController.cs
+++ Assets/Code/Controllers/CameraController.cs
@@ -2,27 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+namespace Esmiylara.Controllers
+{
+    public class CameraController : MonoBehaviour
+    {
+        public GameObject Target;
 
-    public GameObject Target;
-
-	// Use this for initialization
-	void Start () {
-        Target = GameObject.Find("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Camera.main.orthographicSize = Screen.height / 2 / 32;
-
-        if(Target != null)
+        // Use this for initialization
+        void Start()
         {
-            transform.position = new Vector3(
-                Target.transform.position.x, Target.transform.position.y, -10);
+            Target = GameObject.Find("Player");
         }
-        else
+
+        // Update is called once per frame
+        void Update()
         {
-            transform.position = new Vector3(0, 0, -10);
+            Camera.main.orthographicSize = (Screen.height / 2) / Constants.Graphics.PixelsPerUnit;
+
+            if (Target != null)
+            {
+                transform.position = new Vector3(
+                    Target.transform.position.x, Target.transform.position.y, -10);
+            }
+            else
+            {
+                transform.position = new Vector3(0, 0, -10);
+            }
         }
-	}
+    }
 }
